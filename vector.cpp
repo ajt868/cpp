6ac0572c91b037fb5204e1e3d1f66b9d87a7460a
Theyagarajan's Macbook Air <theyagarajansmacbookair@TheyagajanssAir.modem>
2025-05-26 22:10:12 +1000

    Added vector files

---
+++ vector.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    cout << nums.at(2) << endl;
+    nums.push_back(100);
+    cout << nums.back() << endl;
+    nums.pop_back();
+    cout << nums.back();
+    
+}
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        vector<int> nums_copy;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.at(i) == val) {
+                nums_copy.push_back(nums.at(i));
+            }
+        }
+        nums = nums_copy;
+
+        return nums.size();
+    }
+};
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+
+    }
+};
